7a91ae93ca7dd7a4e603cab89b9f2e9f0432510c
kataru24 <katarukashimo24@gmail.com>
2022-03-08 12:02:05 +0100

    Verzion doh

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,3 +50,24 @@ function toggleSkills(){
 skillsHeader.forEach((el) =>{
     el.addEventListener('click', toggleSkills)
 })
+
+// QUALIFICATION TABS HERE
+
+const tabs = document.querySelectorAll('[data-target]'),
+    tabsContents = document.querySelectorAll('[data-content]')
+
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabsContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
